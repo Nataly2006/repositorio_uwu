e4f880db8ddd3f5d643b13b3c5d89645f0ffdc42
nataly2006 <natalyepul98@gmail.com>
2022-08-12 12:33:04 -0400

    Desafío de código: Predecir funciones

---
+++ Nataly_Epul_jsPredicción.js
@@ -0,0 +1,17 @@
+<h1>Predicción1</h1>
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+  //el console.log imprime naci en 1980  
+<h2>Predicción2</h2>
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+//el console.log indicara naci en 
+<h1>predicción3</h1>  
+  function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+// el console.log indicara que sum es 30    
+
+
