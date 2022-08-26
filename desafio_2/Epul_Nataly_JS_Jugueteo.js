f9ddfc90c7085b8122d8a4b9c002cd28158fbd20
nataly2006 <natalyepul98@gmail.com>
2022-08-26 10:24:36 -0400

    Desafío_de_código

---
+++ desafio_2/Epul_Nataly_JS_Jugueteo.js
@@ -0,0 +1,25 @@
+var alturaNiño = 160
+function muestraSiElNiñoPuedeSubirALaMontañaRusa { alturaNiño > 52 ="¡Súbete, chico!"
+ alturaNiño < 52 = "Lo siento, chico. Tal vez el próximo año” }
+
+console.log
+
+
+/* primer intento no funciona TwT */
+
+var alturaNiño = 60 
+
+
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño)
+{ 
+    var parametro1 = "¡Súbete, chico!"
+    var parametro2 = "Lo siento, chico. Tal vez el próximo año"
+    if (alturaNiño > 52 ) {
+            console.log(parametro1)
+    }
+    else {
+                 console.log(parametro2)
+    }
+    console.log(alturaNiño) ; 
+}
+/* sengundo intento y no funciona ,vamos a morir :( */
