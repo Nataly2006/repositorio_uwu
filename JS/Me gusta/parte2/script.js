65bb9b2cd11e2229d5bc23a4135912ec5109e74c
Nataly2006 <natalyepul98@gmail.com>
2023-03-31 10:30:10 -0300

    codigos de hoy

--- JS/Me gusta/parte2/script.js
+++ JS/Me gusta/parte2/script.js
@@ -1,10 +1,1 @@
-var btn2 = 12;
-var countElement = document.querySelector("#btn2")
 
-console.log(countElement);
-
-function add1() {
-    btn2++;
-    countElement.innerText = btn2 +"like(s)";
-    console.log(btn2);
-}
