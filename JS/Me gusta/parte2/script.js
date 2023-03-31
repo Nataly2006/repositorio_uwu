be30d4d39d52debb504adfe615be45b8624ab79e
Nataly2006 <natalyepul98@gmail.com>
2023-03-31 15:52:43 -0300

    fgd

--- JS/Me gusta/parte2/script.js
+++ JS/Me gusta/parte2/script.js
@@ -1,1 +1,11 @@
+var btn = [9, 12, 9];
+span = [
+        document.querySelector("#post-1"),
+        document.querySelector("#post-2"),
+        document.querySelector("#post-3")
+]
 
+function btn(id) {
+    btn[id]++;
+    span[id].innerHTML = btn[id] + "likes";
+}
