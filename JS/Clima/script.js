590fdb120f309a97c99a53809e24a7d0c73d7a81
nataly <natalyepul98@gmail.com>
2023-04-21 14:06:49 -0400

    wiu

--- JS/Clima/script.js
+++ JS/Clima/script.js
@@ -1,4 +1,11 @@
+console.log("page loading...");
+
+var subDiv = document.querySelector(".sub-Cookie");
+
 function message() {
     alert(" Loading weather report (˵ •̀ ᴗ - ˵ ) ✧ ")
 }
-
+/* La funcion que "Elimina" el sub- Cookie */
+function accept() {
+    subDiv.remove();
+}
