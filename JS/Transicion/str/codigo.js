d6e7bbab0e9447e833c6b018cc0318ddbbf291eb
nataly <natalyepul98@gmail.com>
2023-05-16 14:11:09 -0400

    js

---
+++ JS/Transicion/str/codigo.js
@@ -0,0 +1,11 @@
+const check = document.querySelector(".checkbox");
+
+check.addEventListener("click", e => {
+  if (e.target.classList.contains("checked")) {
+    e.target.classList.remove("checked");
+    } else {
+    e.target.classList.add("checked");
+    }
+});
+//# sourceURL=pen.js
+    
